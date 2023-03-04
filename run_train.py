e821a99d3b771f15045b29ec3eacc5d5d2987718
tianshao1992 <tianyuannn@163.com>
2023-03-04 17:51:36 +0800

    modify valid

--- run_train.py
+++ run_train.py
@@ -31,10 +31,10 @@ lrG = 0.0005
 # decay learning rate?
 decayLr = True
 # channel exponent to control network size
-expo = 5
+expo = 6
 # data set config
 # prop = None  # by default, use all from "../data/train"
-prop = [10000, 0.75, 0, 0.25]  # mix data from multiple directories
+prop = [10000, 0.0, 0, 1.0]  # mix data from multiple directories
 # save txt files with per epoch loss?
 saveL1 = True
 # model type
@@ -42,126 +42,128 @@ net = 'UNet'
 
 ##########################
 
-work_path = os.path.join('work', net, str(prop))
-data_path = os.path.join('data')
+for p in ((800,)):
+    prop[0] = p
 
-prefix = work_path + '-expo-' + str(expo) + "/"
-print("Output prefix: {}".format(prefix))
+    work_path = os.path.join('work', net, str(prop))
+    data_path = os.path.join('data')
 
-dropout = 0.  # note, the original runs from https://arxiv.org/abs/1810.08217 used slight dropout, but the effect is minimal; conv layers "shouldn't need" dropout, hence set to 0 here.
-doLoad = ""  # optional, path to pre-trained model
-print("net: {}".format(net))
-print("LR: {}".format(lrG))
-print("LR decay: {}".format(decayLr))
-print("Iterations: {}".format(iterations))
-print("Dropout: {}".format(dropout))
+    prefix = work_path + '-expo-' + str(expo) + "/"
+    print("Output prefix: {}".format(prefix))
 
-##########################
+    dropout = 0.  # note, the original runs from https://arxiv.org/abs/1810.08217 used slight dropout, but the effect is minimal; conv layers "shouldn't need" dropout, hence set to 0 here.
+    doLoad = ""  # optional, path to pre-trained model
+    print("net: {}".format(net))
+    print("LR: {}".format(lrG))
+    print("LR decay: {}".format(decayLr))
+    print("Iterations: {}".format(iterations))
+    print("Dropout: {}".format(dropout))
 
-seed = random.randint(0, 2 ** 32 - 1)
-print("Random seed: {}".format(seed))
-random.seed(seed)
-np.random.seed(seed)
-paddle.seed(seed)
-# paddle.cuda.manual_seed_all(seed)
-# torch.backends.cudnn.deterministic=True # warning, slower
+    ##########################
 
-# create pytorch data object with dfp dataset
-train_path = os.path.join(data_path, 'train/')
-valid_path = os.path.join(data_path, 'test/')
-data = read_data.TurbDataset(prop, shuffle=1, dataDir=train_path, dataDirTest=valid_path)
-trainLoader = DataLoader(data, batch_size=batch_size, shuffle=True, drop_last=True)
-print("Training batches: {}".format(len(trainLoader)))
-dataValidation = read_data.ValiDataset(data)
-validLoader = DataLoader(dataValidation, batch_size=batch_size, shuffle=False, drop_last=True)
-print("Validation batches: {}".format(len(validLoader)))
+    seed = random.randint(0, 2 ** 32 - 1)
+    print("Random seed: {}".format(seed))
+    random.seed(seed)
+    np.random.seed(seed)
+    paddle.seed(seed)
+    # paddle.cuda.manual_seed_all(seed)
+    # torch.backends.cudnn.deterministic=True # warning, slower
 
-# setup training
-epochs = int(iterations / len(trainLoader) + 0.5)
-if 'UNet' in net:
-    net_model = UNet2d(channelExponent=expo, dropout=dropout)
-elif 'FNO' in net:
-    net_model = FNO2d(in_dim=3, out_dim=3, modes=(32, 32), width=32, depth=4, steps=1, padding=4, activation='gelu')
-elif 'Transformer' in net:
-    import yaml
+    # create pytorch data object with dfp dataset
+    train_path = os.path.join(data_path, 'train/')
+    valid_path = os.path.join(data_path, 'test/')
+    data = read_data.TurbDataset(prop, shuffle=1, dataDir=train_path, dataDirTest=valid_path)
+    trainLoader = DataLoader(data, batch_size=batch_size, shuffle=True, drop_last=True)
+    print("Training batches: {}".format(len(trainLoader)))
+    dataValidation = read_data.ValiDataset(data)
+    validLoader = DataLoader(dataValidation, batch_size=batch_size, shuffle=False, drop_last=True)
+    print("Validation batches: {}".format(len(validLoader)))
 
-    with open(os.path.join('transformer_config.yml')) as f:
-        config = yaml.full_load(f)
-    config = config['Transformer']
-    net_model = FourierTransformer2D(**config)
+    # setup training
+    epochs = int(iterations / len(trainLoader) + 0.5)
+    if 'UNet' in net:
+        net_model = UNet2d(channelExponent=expo, dropout=dropout)
+    elif 'FNO' in net:
+        net_model = FNO2d(in_dim=3, out_dim=3, modes=(32, 32), width=32, depth=4, steps=1, padding=4, activation='gelu')
+    elif 'Transformer' in net:
+        import yaml
 
-print(net_model)  # print full net
-model_parameters = filter(lambda p: ~p.stop_gradient, net_model.parameters())
-params = sum([np.prod(p.shape) for p in model_parameters])
-print("Initialized TurbNet with {} trainable params ".format(params))
+        with open(os.path.join('transformer_config.yml')) as f:
+            config = yaml.full_load(f)
+        config = config['Transformer']
+        net_model = FourierTransformer2D(**config)
 
-if len(doLoad) > 0:
-    net_model.load_state_dict(paddle.load(doLoad))
-    print("Loaded model " + doLoad)
+    print(net_model)  # print full net
+    model_parameters = filter(lambda p: ~p.stop_gradient, net_model.parameters())
+    params = sum([np.prod(p.shape) for p in model_parameters])
+    print("Initialized TurbNet with {} trainable params ".format(params))
 
-criterionL1 = nn.L1Loss()
-optimizerG = optim.Adam(parameters=net_model.parameters(), learning_rate=lrG, beta1=0.5, beta2=0.999, weight_decay=0.0)
+    if len(doLoad) > 0:
+        net_model.load_state_dict(paddle.load(doLoad))
+        print("Loaded model " + doLoad)
 
-##########################
+    criterionL1 = nn.L1Loss()
+    optimizerG = optim.Adam(parameters=net_model.parameters(), learning_rate=lrG, beta1=0.5, beta2=0.999,
+                            weight_decay=0.0)
 
-for epoch in range(epochs):
-    print("Starting epoch {} / {}".format((epoch + 1), epochs))
+    ##########################
 
-    net_model.train()
-    L1_accum = 0.0
-    for i, traindata in enumerate(trainLoader, 0):
-        inputs, targets = traindata
+    for epoch in range(epochs):
+        print("Starting epoch {} / {}".format((epoch + 1), epochs))
 
-        # compute LR decay
-        if decayLr:
-            currLr = utils.computeLR(epoch, epochs, lrG * 0.1, lrG)
-            optimizerG.set_lr(currLr)
+        net_model.train()
+        L1_accum = 0.0
+        for i, traindata in enumerate(trainLoader, 0):
+            inputs, targets = traindata
 
-        optimizerG.clear_grad()
-        gen_out = net_model(inputs)
+            # compute LR decay
+            if decayLr:
+                currLr = utils.computeLR(epoch, epochs, lrG * 0.1, lrG)
+                optimizerG.set_lr(currLr)
 
-        lossL1 = criterionL1(gen_out, targets)
-        lossL1.backward()
+            optimizerG.clear_grad()
+            gen_out = net_model(inputs)
 
-        optimizerG.step()
+            lossL1 = criterionL1(gen_out, targets)
+            lossL1.backward()
 
-        lossL1viz = lossL1.item()
-        L1_accum += lossL1viz
+            optimizerG.step()
 
+            lossL1viz = lossL1.item()
+            L1_accum += lossL1viz
 
+        # validation
+        net_model.eval()
+        L1val_accum = 0.0
+        for i, validata in enumerate(validLoader, 0):
+            inputs, targets = validata
+            with paddle.no_grad():
+                outputs = net_model(inputs)
+                lossL1 = criterionL1(outputs, targets)
+                L1val_accum += lossL1.item()
 
-    # validation
-    net_model.eval()
-    L1val_accum = 0.0
-    for i, validata in enumerate(validLoader, 0):
-        inputs, targets = validata
-        with paddle.no_grad():
-            outputs = net_model(inputs)
-            lossL1 = criterionL1(outputs, targets)
-            L1val_accum += lossL1.item()
+        if epoch % 10 == 0:
+            input_ndarray = inputs.numpy()[0]
+            v_norm = (np.max(np.abs(input_ndarray[0, :, :])) ** 2 + np.max(np.abs(input_ndarray[1, :, :])) ** 2) ** 0.5
 
-    if epoch % 10 == 0:
-        input_ndarray = inputs.numpy()[0]
-        v_norm = (np.max(np.abs(input_ndarray[0, :, :])) ** 2 + np.max(np.abs(input_ndarray[1, :, :])) ** 2) ** 0.5
+            outputs_denormalized = data.denormalize(outputs.numpy()[0], v_norm)
+            targets_denormalized = data.denormalize(targets.numpy()[0], v_norm)
+            utils.makeDirs([prefix + "results_train"])
+            utils.imageOut(prefix + "results_train/epoch{}_{}".format(epoch, i), outputs_denormalized,
+                           targets_denormalized,
+                           saveTargets=True)
 
-        outputs_denormalized = data.denormalize(outputs.numpy()[0], v_norm)
-        targets_denormalized = data.denormalize(targets.numpy()[0], v_norm)
-        utils.makeDirs([prefix + "results_train"])
-        utils.imageOut(prefix + "results_train/epoch{}_{}".format(epoch, i), outputs_denormalized,
-                       targets_denormalized,
-                       saveTargets=True)
+        # data for graph plotting
+        L1_accum /= len(trainLoader)
+        L1val_accum /= len(validLoader)
+        if saveL1:
+            if epoch == 0:
+                utils.resetLog(prefix + "L1.txt")
+                utils.resetLog(prefix + "L1val.txt")
+            utils.log(prefix + "L1.txt", "{} ".format(L1_accum), False)
+            utils.log(prefix + "L1val.txt", "{} ".format(L1val_accum), False)
 
-    # data for graph plotting
-    L1_accum /= len(trainLoader)
-    L1val_accum /= len(validLoader)
-    if saveL1:
-        if epoch == 0:
-            utils.resetLog(prefix + "L1.txt")
-            utils.resetLog(prefix + "L1val.txt")
-        utils.log(prefix + "L1.txt", "{} ".format(L1_accum), False)
-        utils.log(prefix + "L1val.txt", "{} ".format(L1val_accum), False)
+        logline = "Epoch: {}, batch-idx: {}, L1: {}, L1_val {}\n".format(epoch, i, L1_accum, L1val_accum)
+        print(logline)
 
-    logline = "Epoch: {}, batch-idx: {}, L1: {}, L1_val {}\n".format(epoch, i, L1_accum, L1val_accum)
-    print(logline)
-
-paddle.save(net_model.state_dict(), prefix + "net_model")
+    paddle.save(net_model.state_dict(), prefix + "net_model")
--- run_valid.py
+++ run_valid.py
@@ -21,144 +21,145 @@ import utils
 from utils import log
 
 dropout = 0.0
-prop = [10000, 0.75, 0, 0.25]
+prop = [10000, 0.0, 0, 1.0]
 net = 'UNet'
-expo = 5
+expo = 6
 ##########################
 
-work_path = os.path.join('work', net, str(prop))
-data_path = os.path.join('data')
+for p in ((100, 200, 400, 1600, 3200, 6400, 12800)):
+    prop[0] = p
 
-suffix = ""  # customize loading & output if necessary
-prefix = work_path + '-expo-' + str(expo) + "/"
-print("Output prefix: {}".format(prefix))
+    work_path = os.path.join('work', net, str(prop))
+    data_path = os.path.join('data')
 
-train_path = os.path.join(data_path, 'train/')
-valid_path = os.path.join(data_path, 'test/')
-dataset = TurbDataset(prop, mode=TurbDataset.TEST, dataDir=train_path, dataDirTest=valid_path)
-# dataset = TurbDataset(None, mode=TurbDataset.TEST, dataDirTest="../data/test/")
-testLoader = DataLoader(dataset, batch_size=1, shuffle=False)
+    suffix = ""  # customize loading & output if necessary
+    prefix = work_path + '-expo-' + str(expo) + "/"
+    print("Output prefix: {}".format(prefix))
 
-if 'UNet' in net:
-    net_model = UNet2d(channelExponent=expo, dropout=dropout)
-elif 'FNO' in net:
-    net_model = FNO2d(in_dim=3, out_dim=3, modes=(32, 32), width=32, depth=4, steps=1, padding=4, activation='gelu')
-elif 'Transformer' in net:
-    import yaml
+    train_path = os.path.join(data_path, 'train/')
+    valid_path = os.path.join(data_path, 'test/')
+    dataset = TurbDataset(prop, mode=TurbDataset.TEST, dataDir=train_path, dataDirTest=valid_path)
+    # dataset = TurbDataset(None, mode=TurbDataset.TEST, dataDirTest="../data/test/")
+    testLoader = DataLoader(dataset, batch_size=1, shuffle=False)
 
-    with open(os.path.join('transformer_config.yml')) as f:
-        config = yaml.full_load(f)
-    config = config['Transformer']
-    net_model = FourierTransformer2D(**config)
-lf = prefix + "testout{}.txt".format(suffix)
-utils.makeDirs([prefix + "results_test"])
-utils.resetLog(lf)
-# loop over different trained models
-avgLoss = 0.
-losses = []
-models = []
+    if 'UNet' in net:
+        net_model = UNet2d(channelExponent=expo, dropout=dropout)
+    elif 'FNO' in net:
+        net_model = FNO2d(in_dim=3, out_dim=3, modes=(32, 32), width=32, depth=4, steps=1, padding=4, activation='gelu')
+    elif 'Transformer' in net:
+        import yaml
 
-for si in range(25):
-    s = chr(96 + si)
-    if (si == 0):
-        s = ""  # check modelG, and modelG + char
-    modelFn = "./" + prefix + "net_model{}{}".format(suffix, s)
-    if not os.path.isfile(modelFn):
-        continue
+        with open(os.path.join('transformer_config.yml')) as f:
+            config = yaml.full_load(f)
+        config = config['Transformer']
+        net_model = FourierTransformer2D(**config)
+    lf = prefix + "testout{}.txt".format(suffix)
+    utils.makeDirs([prefix + "results_test"])
+    utils.resetLog(lf)
+    # loop over different trained models
+    avgLoss = 0.
+    losses = []
+    models = []
 
-    models.append(modelFn)
-    log(lf, "Loading " + modelFn)
-    net_model.set_state_dict(paddle.load(modelFn))
-    log(lf, "Loaded " + modelFn)
-    # netG.cuda()
+    for si in range(25):
+        s = chr(96 + si)
+        if (si == 0):
+            s = ""  # check modelG, and modelG + char
+        modelFn = "./" + prefix + "net_model{}{}".format(suffix, s)
+        if not os.path.isfile(modelFn):
+            continue
 
-    criterionL1 = nn.L1Loss()
-    # criterionL1.cuda()
-    L1val_accum = 0.0
-    L1val_dn_accum = 0.0
-    lossPer_p_accum = 0
-    lossPer_v_accum = 0
-    lossPer_accum = 0
+        models.append(modelFn)
+        log(lf, "Loading " + modelFn)
+        net_model.set_state_dict(paddle.load(modelFn))
+        log(lf, "Loaded " + modelFn)
+        # netG.cuda()
 
-    net_model.eval()
+        criterionL1 = nn.L1Loss()
+        # criterionL1.cuda()
+        L1val_accum = 0.0
+        L1val_dn_accum = 0.0
+        lossPer_p_accum = 0
+        lossPer_v_accum = 0
+        lossPer_accum = 0
 
-    for i, data in enumerate(testLoader, 0):
-        inputs, targets = data
-        with paddle.no_grad():
-            outputs = net_model(inputs)
-        outputs = outputs[0]
-        targets = targets[0]
+        net_model.eval()
 
-        lossL1 = criterionL1(outputs, targets)
-        L1val_accum += lossL1.item()
+        for i, data in enumerate(testLoader, 0):
+            inputs, targets = data
+            with paddle.no_grad():
+                outputs = net_model(inputs)
+            outputs = outputs[0]
+            targets = targets[0]
 
-        outputs = np.array(outputs)
-        targets = np.array(targets)
+            lossL1 = criterionL1(outputs, targets)
+            L1val_accum += lossL1.item()
 
-        # precentage loss by ratio of means which is same as the ratio of the sum
-        lossPer_p = np.sum(np.abs(outputs[0] - targets[0])) / np.sum(np.abs(targets[0]))
-        lossPer_v = (np.sum(np.abs(outputs[1] - targets[1])) + np.sum(np.abs(outputs[2] - targets[2]))) \
-                    / (np.sum(np.abs(targets[1])) + np.sum(np.abs(targets[2])))
-        lossPer = np.sum(np.abs(outputs - targets)) / np.sum(np.abs(targets))
-        lossPer_p_accum += lossPer_p.item()
-        lossPer_v_accum += lossPer_v.item()
-        lossPer_accum += lossPer.item()
+            outputs = np.array(outputs)
+            targets = np.array(targets)
 
-        log(lf, "Test sample %d" % i)
-        log(lf, "    pressure:  abs. difference, ratio: %f , %f " % (np.sum(np.abs(outputs[0] - targets[0])),
-                                                                     lossPer_p.item()))
-        log(lf, "    velocity:  abs. difference, ratio: %f , %f " % (np.sum(np.abs(outputs[1] - targets[1])) +
-                                                                     np.sum(np.abs(outputs[2] - targets[2])),
-                                                                     lossPer_v.item()))
-        log(lf, "    aggregate: abs. difference, ratio: %f , %f " % (np.sum(np.abs(outputs - targets)),
-                                                                     lossPer.item()))
+            # precentage loss by ratio of means which is same as the ratio of the sum
+            lossPer_p = np.sum(np.abs(outputs[0] - targets[0])) / np.sum(np.abs(targets[0]))
+            lossPer_v = (np.sum(np.abs(outputs[1] - targets[1])) + np.sum(np.abs(outputs[2] - targets[2]))) \
+                        / (np.sum(np.abs(targets[1])) + np.sum(np.abs(targets[2])))
+            lossPer = np.sum(np.abs(outputs - targets)) / np.sum(np.abs(targets))
+            lossPer_p_accum += lossPer_p.item()
+            lossPer_v_accum += lossPer_v.item()
+            lossPer_accum += lossPer.item()
 
-        # Calculate the norm
-        input_ndarray = inputs.numpy()[0]
-        v_norm = (np.max(np.abs(input_ndarray[0, :, :])) ** 2 + np.max(np.abs(input_ndarray[1, :, :])) ** 2) ** 0.5
+            log(lf, "Test sample %d" % i)
+            log(lf, "    pressure:  abs. difference, ratio: %f , %f " % (np.sum(np.abs(outputs[0] - targets[0])),
+                                                                         lossPer_p.item()))
+            log(lf, "    velocity:  abs. difference, ratio: %f , %f " % (np.sum(np.abs(outputs[1] - targets[1])) +
+                                                                         np.sum(np.abs(outputs[2] - targets[2])),
+                                                                         lossPer_v.item()))
+            log(lf, "    aggregate: abs. difference, ratio: %f , %f " % (np.sum(np.abs(outputs - targets)),
+                                                                         lossPer.item()))
 
-        outputs_denormalized = dataset.denormalize(outputs, v_norm)
-        targets_denormalized = dataset.denormalize(targets, v_norm)
+            # Calculate the norm
+            input_ndarray = inputs.numpy()[0]
+            v_norm = (np.max(np.abs(input_ndarray[0, :, :])) ** 2 + np.max(np.abs(input_ndarray[1, :, :])) ** 2) ** 0.5
 
-        # denormalized error
-        outputs_denormalized_comp = np.array([outputs_denormalized])
-        # outputs_denormalized_comp=torch.from_numpy(outputs_denormalized_comp)
-        targets_denormalized_comp = np.array([targets_denormalized])
-        # targets_denormalized_comp=torch.from_numpy(targets_denormalized_comp)
+            outputs_denormalized = dataset.denormalize(outputs, v_norm)
+            targets_denormalized = dataset.denormalize(targets, v_norm)
 
-        # targets_denormalized_comp, outputs_denormalized_comp = targets_denormalized_comp.float().cuda(), outputs_denormalized_comp.float().cuda()
+            # denormalized error
+            outputs_denormalized_comp = np.array([outputs_denormalized])
+            # outputs_denormalized_comp=torch.from_numpy(outputs_denormalized_comp)
+            targets_denormalized_comp = np.array([targets_denormalized])
+            # targets_denormalized_comp=torch.from_numpy(targets_denormalized_comp)
 
-        # outputs_dn.data.resize_as_(outputs_denormalized_comp).copy_(outputs_denormalized_comp)
-        # targets_dn.data.resize_as_(targets_denormalized_comp).copy_(targets_denormalized_comp)
+            # targets_denormalized_comp, outputs_denormalized_comp = targets_denormalized_comp.float().cuda(), outputs_denormalized_comp.float().cuda()
 
-        outputs_dn = paddle.to_tensor(outputs_denormalized_comp)
-        targets_dn = paddle.to_tensor(targets_denormalized_comp)
+            # outputs_dn.data.resize_as_(outputs_denormalized_comp).copy_(outputs_denormalized_comp)
+            # targets_dn.data.resize_as_(targets_denormalized_comp).copy_(targets_denormalized_comp)
 
-        lossL1_dn = criterionL1(outputs_dn, targets_dn)
-        L1val_dn_accum += lossL1_dn.item()
+            outputs_dn = paddle.to_tensor(outputs_denormalized_comp)
+            targets_dn = paddle.to_tensor(targets_denormalized_comp)
 
-        # write output image, note - this is currently overwritten for multiple models
+            lossL1_dn = criterionL1(outputs_dn, targets_dn)
+            L1val_dn_accum += lossL1_dn.item()
 
-        utils.imageOut(prefix + "results_test/" + "%04d" % (i), outputs, targets, normalize=False,
-                       saveMontage=True)  # write normalized with error
-        # os.chdir("../")
+            # write output image, note - this is currently overwritten for multiple models
+            utils.imageOut(prefix + "results_test/" + "%04d" % (i), outputs, targets, normalize=False,
+                           saveMontage=True)  # write normalized with error
 
-    log(lf, "\n")
-    L1val_accum /= len(testLoader)
-    lossPer_p_accum /= len(testLoader)
-    lossPer_v_accum /= len(testLoader)
-    lossPer_accum /= len(testLoader)
-    L1val_dn_accum /= len(testLoader)
-    log(lf, "Loss percentage (p, v, combined): %f %%    %f %%    %f %% " %
-        (lossPer_p_accum * 100, lossPer_v_accum * 100, lossPer_accum * 100))
-    log(lf, "L1 error: %f" % (L1val_accum))
-    log(lf, "Denormalized error: %f" % (L1val_dn_accum))
-    log(lf, "\n")
+        log(lf, "\n")
+        L1val_accum /= len(testLoader)
+        lossPer_p_accum /= len(testLoader)
+        lossPer_v_accum /= len(testLoader)
+        lossPer_accum /= len(testLoader)
+        L1val_dn_accum /= len(testLoader)
+        log(lf, "Loss percentage (p, v, combined): %f %%    %f %%    %f %% " %
+            (lossPer_p_accum * 100, lossPer_v_accum * 100, lossPer_accum * 100))
+        log(lf, "L1 error: %f" % (L1val_accum))
+        log(lf, "Denormalized error: %f" % (L1val_dn_accum))
+        log(lf, "\n")
 
-    avgLoss += lossPer_accum
-    losses.append(lossPer_accum)
+        avgLoss += lossPer_accum
+        losses.append(lossPer_accum)
 
-if len(losses) > 1:
-    avgLoss /= len(losses)
-    lossStdErr = np.std(losses) / math.sqrt(len(losses))
-    log(lf, "Averaged relative error and std dev across models:   %f , %f " % (avgLoss, lossStdErr))
+    if len(losses) > 1:
+        avgLoss /= len(losses)
+        lossStdErr = np.std(losses) / math.sqrt(len(losses))
+        log(lf, "Averaged relative error and std dev across models:   %f , %f " % (avgLoss, lossStdErr))
